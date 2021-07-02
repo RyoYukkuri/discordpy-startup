c45806248e6eea25a53af8a49b48a9c109726db2
RyoYukkuri <77970413+RyoYukkuri@users.noreply.github.com>
2021-07-02 14:24:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,19 +1,6 @@
 from discord.ext import commands
 import os
 import traceback
-
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-@bot.command()
-async def neko(ctx):
-    await ctx.send('にゃーん')
-
-
-bot.run(token)
-
-
-
 import discord
 from googletrans import Translator
 
